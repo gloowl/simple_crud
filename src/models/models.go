6adb441a6edc3a95330e3155d5b01acccd9e9245
gloowl <blubsanoff777@gmail.com>
2025-09-08 20:31:18 +0800

    add: добавил файл с моделями данных

---
+++ src/models/models.go
@@ -0,0 +1,54 @@
+package models
+
+import "time"
+
+// Herb - трава
+type Herb struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	LatinName   string    `json:"latin_name"`
+	Description string    `json:"description"`
+	IsPoisonous bool      `json:"is_poisonous"`
+	ImagePath   string    `json:"image_path"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+// Region - регион
+type Region struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+// UsageType - тип использования травы
+type UsageType struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Usage - описание того как можно использовать траву
+type Usage struct {
+	ID          int    `json:"id"`
+	HerbID      int    `json:"herb_id"`
+	UsageTypeID int    `json:"usage_type_id"`
+	Description string `json:"description"`
+
+	HerbName      string `json:"herb_name,omitempty"`
+	UsageTypeName string `json:"usage_type_name,omitempty"`
+}
+
+// HerbRegion - связь многое ко многому
+type HerbRegion struct {
+	HerbID   int `json:"herb_id"`
+	RegionID int `json:"region_id"`
+
+	HerbName   string `json:"herb_name,omitempty"`
+	RegionName string `json:"region_name,omitempty"`
+}
+
+// Структура для ответа
+type HerbWithDetails struct {
+	Herb    Herb     `json:"herb"`
+	Regions []Region `json:"regions"`
+	Usages  []Usage  `json:"usages"`
+}
